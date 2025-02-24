88371998225ea5585c0e3948f646714ef66d83ab
Sheetal Gadodiya <paragagrawal0@gmail.com>
2025-02-24 22:42:42 +0530

    ability to handle concurrent connections

--- app/server.go
+++ app/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -10,25 +12,35 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here! ")
 
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go MessageHandler(conn)
+	}
+}
+
+func MessageHandler(conn net.Conn) {
 	for {
 		data := make([]byte, 1024)
 		_, err := conn.Read(data)
+		if errors.Is(err, io.EOF) {
+			break
+		}
 		if err != nil {
 			fmt.Println("Error reading: ", err.Error())
 			os.Exit(1)
 		}
 		conn.Write([]byte("+PONG\r\n"))
 	}
+	conn.Close()
 }
